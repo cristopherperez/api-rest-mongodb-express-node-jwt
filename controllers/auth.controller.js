d771b113d75811a695f6bc9399d1ed3eb7c38b58
Cristopher Perez <cristopher@rapihogar.com>
2023-03-21 12:30:14 -0300

    feat: api rest

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/User.js";
 import jwt from "jsonwebtoken";
+import { generateRefreshToken, generateToken } from "../utils/tokenManager.js";
 
 export const register = async (req, res) => {
   const { email, password } = req.body;
@@ -38,10 +39,51 @@ export const login = async (req, res) => {
       return res.status(403).json({ error: "Password incorrecto" });
 
     //Generar token con JWT
-    const token = jwt.sign({ uid: user.id }, process.env.JWT_SECRET);
-    return res.json({ ok: token });
+    const { token, expiresIn } = generateToken(user.id);
+    generateRefreshToken(user.id, res);
+
+    return res.json({ token, expiresIn });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "error de servidor" });
   }
 };
+
+export const infoUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.uid).lean();
+    return res.json({ email: user.email, uid: user.id });
+  } catch (error) {
+    return res.status(500).json({ error: "error de server" });
+  }
+};
+
+export const refreshToken = (req, res) => {
+  try {
+    const refreshTokenCookie = req.cookies.refreshToken;
+    if (!refreshTokenCookie) throw new Error("No existe el token");
+
+    const { uid } = jwt.verify(refreshTokenCookie, process.env.JWT_REFRESH); //el segundo argumento es la palabra secreta, aparece como shhh en la documentacion
+    const { token, expiresIn } = generateToken(uid);
+
+    return res.json({ token, expiresIn });
+
+  } catch (error) {
+    console.log(error);
+    const TokenVerificationErrors = {
+      "invalid signature": "La firma del JWT no es válida",
+      "jwt expired": "JWT expirado",
+      "invalid token": "Token no válido",
+      "No Bearer": "Utiliza formato Bearer",
+      "jwt malformed": "JWT formato no válido",
+    };
+    return res
+      .status(401)
+      .send({ error: TokenVerificationErrors[error.message] });
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie('refreshToken')
+  res.json({ok: true})
+}
---
+++ middlewares/requireToken.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+export const requireToken = (req, res, next) => {
+  try {
+    let token = req.headers?.authorization;
+    if (!token) throw new Error("No existe el token en el header usa Bearer");
+    token = token.split(" ")[1];
+    const { uid } = jwt.verify(token, process.env.JWT_SECRET); //el segundo argumento es la palabra secreta, aparece como shhh en la documentacion
+    req.uid = uid;
+
+    next();
+  } catch (error) {
+    console.log(error.message);
+    const TokenVerificationErrors = {
+      "invalid signature": "La firma del JWT no es válida",
+      "jwt expired": "JWT expirado",
+      "invalid token": "Token no válido",
+      "No Bearer": "Utiliza formato Bearer",
+      "jwt malformed":  "JWT formato no válido"
+    };
+    return res
+      .status(401)
+      .send({ error: TokenVerificationErrors[error.message] });
+  }
+};
