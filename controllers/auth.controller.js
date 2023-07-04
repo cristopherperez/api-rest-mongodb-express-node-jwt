b115a77c9f5b1eb683e7b980da448886704ad9ea
Cristopher Perez <cristopher@rapihogar.com>
2023-07-03 22:13:39 -0300

    feat: create login, jwt, register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,43 @@
-// import { User } from "../models/User.js";
-// import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
 // import { generateRefreshToken, generateToken } from "../utils/tokenManager.js";
 
-export const register = (req, res) => {
-  console.log(req.body);
-  res.json({ ok: "register" });
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) throw { code: 11000 };
+
+    user = new User({ email, password });
+    await user.save();
+    return res.status(201).json({ ok: true });
+  } catch (error) {
+    console.log(error);
+    if (error.code === 11000) {
+      return res.status(400).json({ error: "Ya existe este usuario" });
+    }
+    return res.status(500).json({ error: "Error del servidor" });
+  }
 };
 
-export const login = (req, res) => {
-  res.json({ ok: "login" });
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (!user) return res.status(403).json({ error: "No existe este usuario" });
+
+    const respuestaPassword = await user.comparePassword(password);
+    if (!respuestaPassword)
+      return res.status(403).json({ error: "Password incorrecto" });
+
+    const token = jwt.sign({uid: user.id}, process.env.JWT_SECRET)
+
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error del servidor" });
+  }
 };
 
 // export const register = async (req, res) => {
